2a06d9821c796c32b558e1b125682f031b5ee537
JuliusMarckArnesto <jmarnestso@gmail.com>
2026-02-25 08:05:36 +0800

    added update and delete

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -67,3 +67,14 @@ class StudentCreateView(CreateView):
     form_class = StudentForm
     template_name = 'student_form.html'
     success_url = reverse_lazy('student-list')
+
+class StudentUpdateView(UpdateView):
+    model = Student
+    form_class = StudentForm
+    template_name = 'student_form.html'
+    success_url = reverse_lazy('student-list')
+
+class StudentDeleteView(DeleteView):
+    model = Student
+    template_name = 'student_del.html'
+    success_url = reverse_lazy('student-list')
